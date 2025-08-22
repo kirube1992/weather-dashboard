fb206dfc433fa24a6718eda5aed0a8413f2f7b1d
kirubel <kirubelaynalem92@gmail.com>
2025-08-22 16:50:22 +0300

    Add locatonSearch component

---
+++ src/components/LocationSearch.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const searchResults = ref([])
+const searchQuery = ref('')
+const error = ref(null)
+const apiKey = 'f764377eac280d889019ea87b28c6bab'
+
+let serachTimeout = null
+
+const emit = defineEmits(['select-location'])
+
+async function getSerachResults() {
+  searchResults.value = null
+  searchQuery.value = []
+  try {
+    const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchQuery.value}&limit=5&appid=${apiKey}`
+    const response = await fetch(apiUrl)
+    const data = response.json()
+
+    if (data.length > 0) {
+      searchResults.value = data
+    } else {
+      error.value = 'No results found.'
+    }
+  } catch (err) {
+    console.error('faliled to get search results:', err)
+    error.value = 'Could not fetch locations.'
+  }
+}
+
+watch(searchQuery, query)
+</script>
